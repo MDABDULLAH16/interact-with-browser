618406ea4b997cdbcea37b00feb2e2cb5efc77e5
MD ABDULLAH <mdabdullah161036@gmail.com>
2023-12-27 23:24:40 +0600

    comment added

--- js/cart-localStorage.js
+++ js/cart-localStorage.js
@@ -17,10 +17,13 @@ const displayProduct = (product, quantity) => {
   productContaienr.appendChild(li);
 };
 
-//getItem From Local Storage 1: step
+//getItem From Local Storage 1: step {cart key hisabe theke jabe, asign korte hobe sudhu value}
 const getStoredShoppinCart = () => {
+  //jei name rakhbo data 'cart';
   const storedCart = localStorage.getItem("cart");
+  //declear a emty object or initial;
   let cart = {};
+  //if cart name kichu thake tahole take parse kore cart variable rakho, nahole return emty cart
   if (storedCart) {
     cart = JSON.parse(storedCart);
   }
@@ -28,17 +31,25 @@ const getStoredShoppinCart = () => {
 };
 
 // setItem to localStorage 2nd step
+// key: cart , value: product , quantity
 const setProductToLocalStorage = (product, quantity) => {
-  const cart = getStoredShoppinCart();
-  cart[product] = quantity;
-  const CartStringify = JSON.stringify(cart);
+  ///get key cart from getStoredShppingCart function
+  const cartValue = getStoredShoppinCart();
+  //set value in the cart value, you have to sign cart[product] and set quantity;
+  //akhane cart akta object. ar object er vitore man pete hole (variable name [jar man dorkar]) = result/ quantity
+  cartValue[product] = quantity;
+  //converted stringigy the cart for set localStorage. becouse nothing allow without string
+  const CartStringify = JSON.stringify(cartValue);
   localStorage.setItem("cart", CartStringify);
 };
 
 // 3rd step: display data from local storage
 const displayProductFromLocalStorage = () => {
   const savedProduct = getStoredShoppinCart();
+  //to access a object value, that which is stay in variable, : variable name[].
+
   for (const product in savedProduct) {
+    //product er man ace saveProduct er vitore, tai man acces korte hole, variable name[product]== product er man pawa jabe
     const quantity = savedProduct[product];
     console.log(product, quantity);
     displayProduct(product, quantity);
