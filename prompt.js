10ba9600344443d1beef5586883cf5fab4d2bf92
MD ABDULLAH <mdabdullah161036@gmail.com>
2023-12-27 19:56:35 +0600

    use confirm and alert

--- prompt.js
+++ prompt.js
@@ -1,1 +1,11 @@
-console.log("prompting cheeking");
+function makeAlert() {
+  alert("hellow mama ki khobor");
+}
+function makeConfirm() {
+  const result = confirm("pres ok / cancel");
+  if (result === true) {
+    alert("mama okay press korco!!");
+  } else {
+    alert("no press korco keno");
+  }
+}
